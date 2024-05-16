65a5df7475177a5aa1c55638e9bede218431c279
Konstantin Korovkin <kastyara555@gmail.com>
2024-05-16 21:11:53 +0300

    DEV-14: ссылка на салон со списка салонов услуги

--- ora-frontend/screens/Procedure/ProcedureSaloonsList/ProcedureSaloon/index.tsx
+++ ora-frontend/screens/Procedure/ProcedureSaloonsList/ProcedureSaloon/index.tsx
@@ -4,6 +4,7 @@ import classNames from "classnames";
 import { Card } from "primereact/card";
 import { usePathname } from "next/navigation";
 import { useDispatch } from "react-redux";
+import Link from "next/link";
 
 import { BASE_URL } from "@/api";
 import Button from "@/components/Button";
@@ -35,24 +36,27 @@ const ProcedureSaloon: FC<ProcedureSaloonProps> = ({ saloonInfo }) => {
   return (
     <Card className={styles.card}>
       <section className={classNames(styles.content, "w-full")}>
-        <div className={classNames("flex", "align-items-center")}>
-          <img
-            src={
-              saloonInfo.mainImage
-                ? BASE_URL.concat(saloonInfo.mainImage)
-                : DEFAULT_PROFILE_IMAGE
-            }
-            alt={saloonInfo.mainImage ?? "Главное изображение салона"}
-            className={classNames(
-              styles.saloonAvatar,
-              "h-4rem",
-              "w-4rem",
-              "shadow-2",
-              "mr-2"
-            )}
-          />
-          <h3 className="ml-2">{saloonInfo.name}</h3>
-        </div>
+        <Link href={`/saloons/${saloonInfo.id}`} className={styles.link}>
+          <div className={classNames("flex", "align-items-center")}>
+            <img
+              src={
+                saloonInfo.mainImage
+                  ? BASE_URL.concat(saloonInfo.mainImage)
+                  : DEFAULT_PROFILE_IMAGE
+              }
+              alt={saloonInfo.mainImage ?? "Главное изображение салона"}
+              className={classNames(
+                styles.saloonAvatar,
+                "h-4rem",
+                "w-4rem",
+                "shadow-2",
+                "mr-2"
+              )}
+            />
+            <h3 className="ml-2">{saloonInfo.name}</h3>
+          </div>
+        </Link>
+
         <Button onClick={handleRecord} className={styles.chooseDataButton}>
           Выбрать дату
         </Button>
