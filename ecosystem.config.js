77135ca47cb7ce13ceb7eb99ade638e6fa34a24d
Konstantin <kastyara555@gmail.com>
2024-09-29 21:02:09 +0300

    конфиг для старта некста в pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         {
             name: 'ora-frontend',
             script: 'npm run start',
-            cwd: './ora-frontend',
+            cwd: '/ora-frontend',
             args: '-p 4000',
             exec_mode: 'cluster',
             instances: 'max'
