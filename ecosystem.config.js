0f33b05cb8db1e1142778af1bf901e525e2b93ff
Konstantin <kastyara555@gmail.com>
2024-10-01 21:47:15 +0300

    script was corrected in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         {
             name: 'ora-frontend',
             cwd: '/ora-frontend',
-            script: './build',
+            script: './node_modules/next/dist/bin/next',
             args: 'start',
             error_file: './logs/service_gateway.err',
             exec_mode: 'cluster',
